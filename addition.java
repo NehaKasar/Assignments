e18e03c130c176d942975e12925e418929da4604
NehaKasar <83545999+NehaKasar@users.noreply.github.com>
2021-05-02 20:45:40 +0530

    Asg1

---
+++ addition.java
@@ -0,0 +1,18 @@
+ public class addition
+ {
+   public static void main(String args[])
+   {
+ 
+	 String s1=args[0];
+	 String s2=args[1];
+     int i=Integer.parseInt(s1);
+	 int j=Integer.parseInt(s2);
+	 int k=i+j;
+	 
+	 System.out.println("first Number" +i);
+	 System.out.println("Second Number" +j);
+	 System.out.println("addition of k is"+k);
+	 
+	 
+  }
+ }
